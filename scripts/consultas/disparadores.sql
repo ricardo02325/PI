22b2672c3556384473025c7d30c79db0a78d2d38
ricardo02325 <rgregorio0.@ucol.mx>
2025-03-02 18:39:19 -0600

    Se crea SQL

--- scripts/consultas/disparadores.sql
+++ scripts/consultas/disparadores.sql
@@ -1,33 +1,20 @@
--- Disparador para sensor de ph
-DELIMITER //
+DELIMITER $$
 
-CREATE TRIGGER insertar_alerta_valor_fuera_de_rango_ph
-AFTER INSERT ON lecturas_sensores
+CREATE TRIGGER FUERA_RANGO_AI
+AFTER INSERT ON lecturas_sensores 
 FOR EACH ROW
 BEGIN
-    DECLARE umbral_bajo FLOAT;
-    DECLARE umbral_alto FLOAT;
-    DECLARE estado_sensor VARCHAR(50);
-
-    -- Definir los umbrales para el sensor PH
-    SET umbral_bajo = 5.0;
-    SET umbral_alto = 9.0;
-
-    -- Obtener el estado del sensor PH desde la tabla 'sensores'
-    SELECT estado INTO estado_sensor
-    FROM sensores
-    WHERE id_sensor = NEW.id_sensor;
-
-    -- Verificar si el valor del sensor PH está fuera del rango
-    IF NEW.id_sensor = 1 AND (NEW.valor < umbral_bajo OR NEW.valor > umbral_alto) THEN
-        -- Insertar una alerta en la tabla 'alertas' con el estado del sensor
-        INSERT INTO alertas (tipo_alerta, descripcion, fecha_hora, estado)
-        VALUES ('Valor fuera de rango', 'Valor fuera de rango del sensor PH', NOW(), estado_sensor);
+    IF NEW.id_sensor = 1 THEN
+        IF NEW.valor < 5.0 THEN
+            INSERT INTO alertas(tipo_alerta, descripcion, fecha_hora, estado)
+            VALUES ('Valor fuera de rango', 'Valor del sensor muy bajo', NOW(),
+                    (SELECT estado FROM sensores WHERE id_sensor = NEW.id_sensor));
+        ELSEIF NEW.valor > 9.0 THEN
+            INSERT INTO alertas(tipo_alerta, descripcion, fecha_hora, estado)
+            VALUES ('Valor fuera de rango', 'Valor del sensor muy alto', NOW(),
+                    (SELECT estado FROM sensores WHERE id_sensor = NEW.id_sensor));
+        END IF;
     END IF;
-END //
+END$$
 
-DELIMITER ;
-
--- Ejemplo de Insert para que se dispare el disparador
-INSERT INTO lecturas_sensores (id_sensor, valor, fecha_hora)
-VALUES (1, 4.5, NOW());  -- En este caso, el valor es 4.5, que está fuera del rango para el sensor PH
+DELIMITER ; 
