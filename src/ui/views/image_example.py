b19b66d02ed1f352c0fabf05b186d9f4c93154b6
ricardo02325 <rgregorio0.@ucol.mx>
2025-04-01 12:17:02 -0600

    Cambios Gregorio

--- src/ui/views/image_example.py
+++ src/ui/views/image_example.py
@@ -18,7 +18,7 @@ class App(customtkinter.CTk):
 
         image_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "test_images")
         self.logo_image = customtkinter.CTkImage(Image.open(os.path.join(image_path, "CustomTkinter_logo_single.png")), size=(26, 26))
-        self.home_image = customtkinter.CTkImage(light_image=Image.open(os.path.join(image_path, "home_dark.png")),
+        self.home_image = customtkinter.CTkImage(light_image=Image.open(os.path.join(image_path, "home.png")),
                                                  dark_image=Image.open(os.path.join(image_path, "home_light.png")), size=(20, 20))
         self.alerts_image = customtkinter.CTkImage(light_image=Image.open(os.path.join(image_path, "chat_dark.png")),
                                                    dark_image=Image.open(os.path.join(image_path, "chat_light.png")), size=(20, 20))
